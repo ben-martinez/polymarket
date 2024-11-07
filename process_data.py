86e214333f5d1a6e5149e8474c411563095ba602
deniselj24 <122917446+deniselj24@users.noreply.github.com>
2024-11-07 04:09:49 -0800

    create pyg dataset

---
+++ process_data.py
@@ -0,0 +1,147 @@
+import json
+import torch
+import torch_geometric
+# from torch_geometric import Data
+from tqdm import tqdm
+from torch_geometric.data import HeteroData
+
+def flatten_dataset(file_path, outfile_users, outfile_markets, outfile_positions):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    output_users = []
+    output_markets = []
+    output_positions = []
+    # make it such that each entry is flat row instead of a nested dict
+    marketPositions = data["data"]["marketPositions"]
+    for i in tqdm(range(len(marketPositions))):
+        pos = marketPositions[i]
+
+        # may contain duplicate user
+        output_users.append({
+            "user_id": pos["user"]["id"],
+            "position_id": pos["id"],
+            "lastTradedTimestamp": pos["user"]["lastTradedTimestamp"],
+            "user_creationTimestamp": pos["user"]["creationTimestamp"],
+            "collateralVolume": pos["user"]["collateralVolume"],
+            "numTrades": pos["user"]["numTrades"],
+            "scaledProfit": pos["user"]["scaledProfit"],
+            "market_id": pos["market"]["id"]
+        })
+
+        output_markets.append({
+            "user_id": pos["user"]["id"],
+            # "position_id": pos["id"],
+            "market_id": pos["market"]["id"]
+        })
+
+        output_positions.append({
+            # "user_id": pos["user"]["id"],
+            "position_id": pos["id"],
+            # "market_id": pos["market"]["id"],
+            "netValue": pos["netValue"],
+            "feesPaid": pos["feesPaid"],
+            "netQuantity": pos["netQuantity"]
+        })
+
+    with open(outfile_users, 'w') as f:
+        json.dump(output_users, f)
+    with open(outfile_markets, 'w') as f:
+        json.dump(output_markets, f)
+    with open(outfile_positions, 'w') as f:
+        json.dump(output_positions, f)
+
+def create_pyg_dataset(users_path, markets_path, positions_path):
+    import pandas
+    users_df = pandas.read_json(users_path)
+    markets_df = pandas.read_json(markets_path)
+    positions_df = pandas.read_json(positions_path)
+
+    # map each node type id to a new id (0 to num_unique_<node_type>)
+    unique_users = users_df["user_id"].unique()
+    # print(unique_users)
+    unique_user_ids = pandas.DataFrame(data={
+        'user_id': unique_users,
+        'user_index': pandas.RangeIndex(len(unique_users)),
+    })
+    df = pandas.merge(users_df, unique_user_ids, left_on='user_id', right_on='user_id', how='left')
+
+    unique_markets = markets_df['market_id'].unique()
+    unique_market_ids = pandas.DataFrame(data={
+        'market_id': unique_markets,
+        'market_index': pandas.RangeIndex(len(unique_markets)),
+    })
+    df = pandas.merge(df, unique_market_ids, left_on='market_id', right_on='market_id', how='left')
+    markets_df = pandas.merge(markets_df, unique_market_ids, left_on='market_id', right_on='market_id', how='left')
+
+    unique_positions = positions_df['position_id'].unique()
+    unique_position_ids = pandas.DataFrame(data={
+        'position_id': unique_positions,
+        'position_index': pandas.RangeIndex(len(unique_positions)),
+    })
+    df = pandas.merge(df, unique_position_ids, left_on='position_id', right_on='position_id', how='left')
+    positions_df = pandas.merge(positions_df, unique_position_ids, left_on='position_id', right_on='position_id', how='left')
+
+    # shape of [2, num_edges]
+    edge_index_users_to_position = []
+    edge_index_position_to_market = []
+
+    for index, entry in df.iterrows():
+        # print(entry)
+        user_index = entry["user_index"]
+        market_index = entry["market_index"]
+        position_index = entry["position_index"]
+        edge_index_users_to_position.append([user_index, position_index])
+        edge_index_position_to_market.append([position_index, market_index])
+
+    # print(edge_index_users_to_position)
+    # print(edge_index_position_to_market)
+
+    # remove the col(s) of user_id, market_id, etc. because they aren't 0-indexed and are confusing
+    users = df[["user_index", "lastTradedTimestamp", "user_creationTimestamp", "collateralVolume", "numTrades", "scaledProfit"]]
+    users = users.groupby("user_index").agg("mean")
+
+    markets = markets_df[["market_index", "user_id"]]
+    # get the number of unique users to be the feature for markets
+    markets = markets.groupby("market_index").agg(lambda x: len(set(x)))
+
+    positions = positions_df[["position_index", "netValue", "feesPaid", "netQuantity"]]
+    positions = positions.groupby("position_index").agg("mean")
+    #unique_markets = markets_df.groupby("market_id").agg("mean")
+    #unique_positions = positions_df.groupby("position_id").agg("mean")
+
+    # Reference: https://pytorch-geometric.readthedocs.io/en/2.6.0/notes/heterogeneous.html
+    data = HeteroData()
+    # shape [num unique users, num user features = 5]
+    data['user'].x = torch.from_numpy(users.values)
+    # shape [num_unique_markets, num market features = 1 (number of unique users betting in it currently)]
+    data['market'].x = torch.from_numpy(markets.values)
+    # shape [num_unique positions, num position features = 3]
+    data['position'].x = torch.from_numpy(positions.values)
+
+    data['user', 'bets', 'position'].edge_index = torch.Tensor(edge_index_users_to_position).T
+    data['position', 'active', 'market'].edge_index = torch.Tensor(edge_index_position_to_market).T
+
+    print(data)
+    """
+        HeteroData(
+      user={ x=[10, 5] },
+      market={ x=[91, 1] },
+      position={ x=[100, 3] },
+      (user, bets, position)={ edge_index=[2, 100] },
+      (position, active, market)={ edge_index=[2, 100] }
+    )"""
+    return data
+
+# Initial data pulled from the API
+file_path = "query_1.json"
+
+users_path = "users.json"
+markets_path = "markets.json"
+positions_path = "positions.json"
+
+flatten_dataset(file_path, users_path, markets_path, positions_path)
+
+pyg_data = create_pyg_dataset(users_path, markets_path, positions_path)#, "users.json", "markets.json", "positions.json")
+
+
